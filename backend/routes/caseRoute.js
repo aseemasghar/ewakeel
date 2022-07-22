4e2c0f97131c4e37f17bbbf4429993e1078c95ef
Aseem Asghar <aseemasghar@gmail.com>
2022-07-22 14:29:45 +0500

    22 july

--- backend/routes/caseRoute.js
+++ backend/routes/caseRoute.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const { getAllCases, createCase, updateCase, deleteCase, getCaseDetails } = require('../controllers/caseController');
-const { isAuthenticateduser,authorizeRoles } = require('../middleware/auth');
+const { isAuthenticateduser } = require('../middleware/auth');
 
 const router = express.Router();
 router.route('/cases').get(getAllCases);
-router.route('/case/new').post(isAuthenticateduser,authorizeRoles('client'),createCase);
-router.route('/case/:id').put(isAuthenticateduser,authorizeRoles('client'),updateCase).delete(isAuthenticateduser,authorizeRoles('client'),deleteCase).get(getCaseDetails);
+router.route('/case/new').post(isAuthenticateduser,createCase);
+router.route('/case/:id').put(isAuthenticateduser,updateCase).delete(isAuthenticateduser,deleteCase).get(getCaseDetails);
 
 module.exports=router
